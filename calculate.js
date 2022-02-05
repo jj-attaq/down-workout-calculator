e5544b5a58c0647558e7701f0b0834228cad71b1
jj_attaq <jj.attaq@tutanota.com>
2022-02-05 05:45:59 +0100

    more experimenting

--- calculate.js
+++ calculate.js
@@ -1,5 +1,7 @@
-var sets = prompt("How many sets do you want to do?");
-for (var count = 0, result = count; count <= sets; ++count) {
-  result = count + result;
-  console.log(result);
+function caluclate() {
+  var sets = document.getElementById("numberOfSets");
+  for (var count = 0, result = count; count <= sets; ++count) {
+    result = count + result;
+    console.log(result);
+  }
 }
