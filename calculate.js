98ca4258f603ca933f7da8b3a79f589cdce62ef0
jj_attaq <jj.attaq@tutanota.com>
2022-02-20 13:46:26 +0100

    html form + js begin

--- calculate.js
+++
@@ -1,7 +0,0 @@
-function caluclate() {
-  var sets = document.getElementById("numberOfSets");
-  for (var count = 0, result = count; count <= sets; ++count) {
-    result = count + result;
-    console.log(result);
-  }
-}
