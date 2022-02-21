f5e96da20cae9f34db4b6150d7984fbe089460fd
jj_attaq <jj.attaq@tutanota.com>
2022-02-21 11:39:10 +0100

    got js working with form in basic way

---
+++ src/js/calculate.js
@@ -0,0 +1,8 @@
+/*
+var sets = prompt("How many sets do you want to do?");
+      for (var count = 0, result = count; count <= sets; ++count) {
+        result = count + result;
+        console.log(result);
+      }
+      alert(`A "down workout" starting at ${sets} sets will result in ${result} repetions.\n \n GET AFTER IT!`);
+      */
