f1918858d475610014bf6e01104b65b7785476c9
jj.attaq <jj.attaq@tutanota.com>
2022-04-22 12:44:43 +0200

    cleaned up for loop

--- src/js/calculate.js
+++ src/js/calculate.js
@@ -1,8 +1,18 @@
-/*
-var sets = prompt("How many sets do you want to do?");
-      for (var count = 0, result = count; count <= sets; ++count) {
-        result = count + result;
-        console.log(result);
-      }
-      alert(`A "down workout" starting at ${sets} sets will result in ${result} repetions.\n \n GET AFTER IT!`);
-      */
+function calculate() {
+  const sets = +document.getElementById('numberOfSets').value; // add a + in front of document to turn into number value, letters will be NaN
+  for (i = 0, result = i; i <= sets; ++i) {
+    result = i + result;
+    // console.log(result);
+  }
+  console.log(typeof sets);
+  //
+  alert(`A "down workout" starting at ${sets} sets will result in ${result} repetions.\n \n GET AFTER IT!`);
+  /*
+  if (typeof document.getElementById('numberOfSets') !== "number") {
+    alert(`Please enter a valid number.`)
+  }
+  else {
+    alert(`A "down workout" starting at ${sets} sets will result in ${result} repetions.\n \n GET AFTER IT!`);
+  }  
+  */
+}
